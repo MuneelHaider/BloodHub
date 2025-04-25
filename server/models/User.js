1bcb821b05f207dbaaafdb8a3633b38a89ffb314
Muneel Haider <muneelhaider@gmail.com>
2025-04-25 10:12:09 +0500

    Corrected Models

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  bloodType: {
+    type: String,
+    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+    required: true
+  },
+  isDonor: {
+    type: Boolean,
+    default: false
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  blocked: {
+    type: Boolean,
+    default: false
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('User', userSchema)
