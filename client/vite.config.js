41341107bd5dadb2757a9a0b5944496e22484134
Muneel Haider <muneelhaider@gmail.com>
2025-05-14 12:18:00 +0500

    Reversed from AWS

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8080,
-    strictPort: true,
-    host: true
-  }
+  plugins: [react()]
 })
